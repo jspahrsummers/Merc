3310342fee361f441a1a0a846faa2dd81eee9e80
Justin Spahr-Summers <justin@jspahrsummers.com>
2020-05-23 17:21:01 +0100

    Movement

---
+++ Assets/FollowController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowController : MonoBehaviour
+{
+    public GameObject followTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var followPosition = followTarget.transform.position;
+        transform.position = new Vector3(followPosition.x, followPosition.y, transform.position.z);
+    }
+}
---
+++ Assets/ShipController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    public float rotateSpeed;
+    public float thrustAcceleration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * thrustAcceleration * Time.deltaTime);
+    }
+}
