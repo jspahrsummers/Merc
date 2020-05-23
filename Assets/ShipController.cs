e014e686b08694489f30b6d289b6e3561f89b1c7
Justin Spahr-Summers <justin@jspahrsummers.com>
2020-05-23 17:51:47 +0100

    Input-actuated movement

--- Assets/ShipController.cs
+++ Assets/ShipController.cs
@@ -1,11 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ShipController : MonoBehaviour
 {
     public float rotateSpeed;
     public float thrustAcceleration;
+
+    public Vector3 m_Velocity;
+
+    public void OnThrust(InputAction.CallbackContext context)
+    {
+        m_Velocity += transform.up * thrustAcceleration * Time.deltaTime;
+    }
+
+    public void OnTurn(InputAction.CallbackContext context)
+    {
+        transform.Rotate(0, 0, context.ReadValue<float>() * rotateSpeed * Time.deltaTime);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +29,6 @@ public class ShipController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.up * thrustAcceleration * Time.deltaTime);
+        transform.Translate(m_Velocity, Space.World);
     }
 }
