60892cc1f6b3c8f53d6f678c9ed904f7abffbd61
Justin Spahr-Summers <justin@jspahrsummers.com>
2020-05-24 23:49:43 +0100

    Fuel that depletes

--- Assets/Gameplay/PlayerShipController.cs
+++ Assets/Gameplay/PlayerShipController.cs
@@ -2,13 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerShipController : MonoBehaviour
 {
     public float turnSpeed;
     public float torque;
     public float thrust;
+    public float fuelConsumption;
+    public float fuelRegeneration;
     public GameObject missilePrefab;
+
+    public Slider fuelBar;
+
+    private float _fuel = 1;
+    private float fuel
+    {
+        get { return _fuel; }
+        set
+        {
+            _fuel = Mathf.Clamp01(value);
+            fuelBar.value = _fuel;
+        }
+    }
 
     private float m_Turning;
     private float m_Thrusting;
@@ -59,6 +75,18 @@ public class PlayerShipController : MonoBehaviour
             rigidbody.rotation += m_Turning * turnSpeed * Time.deltaTime;
         }
 
-        rigidbody.AddRelativeForce(Vector2.up * m_Thrusting * thrust * Time.deltaTime);
+        if (m_Thrusting > 0)
+        {
+            var neededFuel = fuelConsumption * Time.deltaTime;
+            var beforeFuel = fuel;
+            fuel -= neededFuel;
+            var consumedFuel = beforeFuel - fuel;
+
+            rigidbody.AddRelativeForce(Vector2.up * m_Thrusting * (consumedFuel / neededFuel) * thrust * Time.deltaTime);
+        }
+        else
+        {
+            fuel += fuelRegeneration * Time.deltaTime;
+        }
     }
 }
