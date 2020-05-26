a47dba4c8a8eb04839449386725bea7521d4108d
Justin Spahr-Summers <justin@jspahrsummers.com>
2020-05-26 19:58:05 +0100

    Create a dumb enemy ship

---
+++ Assets/Gameplay/AI/AIShipController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIShipController : MonoBehaviour
+{
+    public float turnSpeed;
+    public float thrust;
+}
