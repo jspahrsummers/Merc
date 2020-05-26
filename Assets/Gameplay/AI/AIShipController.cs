a87a1ffd0bf2033ef854a30ad0f850a859f99a27
Justin Spahr-Summers <justin@jspahrsummers.com>
2020-05-26 20:39:07 +0100

    Basic AI deceleration behavior

--- Assets/Gameplay/AI/AIShipController.cs
+++ Assets/Gameplay/AI/AIShipController.cs
@@ -6,4 +6,44 @@ public class AIShipController : MonoBehaviour
 {
     public float turnSpeed;
     public float thrust;
+    public float speedTolerance;
+    public float rotationTolerance;
+
+    private new Rigidbody2D rigidbody => GetComponent<Rigidbody2D>();
+
+    void Start()
+    {
+        StartCoroutine(DecelerateAndStop());
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log($"Collision between {this} and {other}");
+        StartCoroutine(DecelerateAndStop());
+    }
+
+    private IEnumerator DecelerateAndStop()
+    {
+        while (Mathf.Abs(rigidbody.velocity.magnitude) > speedTolerance)
+        {
+            yield return new WaitForFixedUpdate();
+
+            float velocityAngle = Vector2.SignedAngle(Vector2.up, rigidbody.velocity);
+            float desiredAngle = velocityAngle + 180;
+            Debug.Log($"Velocity angled at {velocityAngle}");
+
+            if (Mathf.Repeat(rigidbody.rotation - desiredAngle, 360) > rotationTolerance)
+            {
+                float newAngle = Mathf.MoveTowardsAngle(rigidbody.rotation, desiredAngle, turnSpeed * Time.fixedDeltaTime);
+                rigidbody.angularVelocity = 0;
+                rigidbody.MoveRotation(newAngle);
+            }
+            else
+            {
+                rigidbody.AddRelativeForce(Vector2.up * thrust * Time.fixedDeltaTime);
+            }
+        }
+
+        Debug.Log("Decelerated to a stop");
+    }
 }
