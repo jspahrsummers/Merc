16a8d2bf36d4982937f818a41d54ae586afbded8
Justin Spahr-Summers <justin@jspahrsummers.com>
2020-05-31 18:26:31 +0100

    Add some disabled ships back to Sol

--- Assets/Gameplay/Ships/AbstractShipController.cs
+++ Assets/Gameplay/Ships/AbstractShipController.cs
@@ -3,8 +3,8 @@
 public abstract class AbstractShipController : MonoBehaviour, IDamageable
 {
     public ShipScriptableObject ship;
+    public Destructible destructible;
 
-    private Destructible destructible;
     private ExplodableController explodable => GetComponent<ExplodableController>();
     public new Rigidbody2D rigidbody => GetComponent<Rigidbody2D>();
 
@@ -13,8 +13,11 @@ public abstract class AbstractShipController : MonoBehaviour, IDamageable
         Debug.Log($"Overriding {rigidbody} mass to {ship.mass}");
         rigidbody.mass = ship.mass;
 
-        destructible = ship.baseDestructible;
-        Debug.Assert(!destructible.IsDestroyed());
+        if (destructible.IsDestroyed())
+        {
+            destructible = ship.baseDestructible;
+            Debug.Assert(!destructible.IsDestroyed());
+        }
     }
 
     public void ApplyDamage(Damage damage)
