1fa7b8b0c64cbc302af7062747c659b6935c4899
Justin Spahr-Summers <justin@jspahrsummers.com>
2020-05-31 13:00:08 +0100

    Refactor projectile lifetime into a coroutine

--- Assets/Gameplay/ProjectileController.cs
+++ Assets/Gameplay/ProjectileController.cs
@@ -1,15 +1,29 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public sealed class ProjectileController : MonoBehaviour
 {
     public ProjectileScriptableObject projectile;
     public GameObject explosionPrefab;
-
-    private float startTime;
+    public new Rigidbody2D rigidbody => GetComponent<Rigidbody2D>();
 
     void Start()
     {
-        startTime = Time.time;
+        if (rigidbody)
+        {
+            rigidbody.mass = projectile.mass;
+        }
+
+        if (projectile.lifetime != Mathf.Infinity)
+        {
+            StartCoroutine(ExplodeAfterLifetime());
+        }
+    }
+
+    private IEnumerator ExplodeAfterLifetime()
+    {
+        yield return new WaitForSeconds(projectile.lifetime);
+        Explode();
     }
 
     private void Explode()
@@ -19,14 +33,8 @@ public sealed class ProjectileController : MonoBehaviour
         {
             Instantiate(explosionPrefab, transform.position, transform.rotation, transform.parent);
         }
-    }
 
-    void Update()
-    {
-        if (Time.time - startTime > projectile.lifetime)
-        {
-            Explode();
-        }
+        // TODO: Damage objects nearby
     }
 
     void OnCollisionEnter2D(Collision2D other)
