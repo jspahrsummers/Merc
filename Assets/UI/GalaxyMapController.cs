474c43cfd7a8803af1de1aed5434467fef27b445
Justin Spahr-Summers <justin@jspahrsummers.com>
2020-06-02 22:37:09 +0100

    Indicate current galaxy in map

--- Assets/UI/GalaxyMapController.cs
+++ Assets/UI/GalaxyMapController.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GalaxyMapController : MonoBehaviour
@@ -7,20 +9,33 @@ public class GalaxyMapController : MonoBehaviour
     public GameObject mapSystemPrefab;
     public float mapScale = 1;
 
+    private Dictionary<string, GameObject> mapSystems = new Dictionary<string, GameObject>();
+
     void Start()
     {
-        var starSystems = StarSystemScriptableObject.AllSystems();
-        foreach (var starSystem in starSystems)
+        foreach (var starSystem in StarSystemScriptableObject.AllSystems())
         {
             Debug.Log($"Adding {starSystem} to map");
 
             var mapSystem = Instantiate<GameObject>(mapSystemPrefab, panel.transform);
+            Debug.Assert(!mapSystems.ContainsKey(starSystem.name));
+            mapSystems.Add(starSystem.name, mapSystem);
 
             var graphic = mapSystem.GetComponent<Graphic>();
             graphic.rectTransform.anchoredPosition = starSystem.galaxyPosition * mapScale;
 
             var label = mapSystem.GetComponentInChildren<Text>();
             label.text = starSystem.name;
+        }
+    }
+
+    void Update()
+    {
+        string currentSystem = SceneManager.GetActiveScene().name;
+        foreach (var mapSystem in mapSystems)
+        {
+            var label = mapSystem.Value.GetComponentInChildren<Text>();
+            label.color = (mapSystem.Key == currentSystem ? Color.yellow : Color.white);
         }
     }
 }
