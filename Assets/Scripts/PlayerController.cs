f5f21309ad105d488ca27fba755285921da967ee
Justin Spahr-Summers <justin@jspahrsummers.com>
2020-08-02 21:10:09 +0100

    Implement basic turning

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+/// <summary>Implements the behaviors of a player (whether or not it is the local player).</summary>
+public sealed class PlayerController : MonoBehaviour
+{
+    private Inputs inputs;
+
+    const float RotationSpeed = 5f;
+
+    void OnEnable()
+    {
+        if (inputs == null)
+        {
+            inputs = new Inputs();
+        }
+
+        inputs.Enable();
+    }
+
+    void OnDisable()
+    {
+        inputs.Disable();
+    }
+
+    void FixedUpdate()
+    {
+        float turn = inputs.Player.Turn.ReadValue<float>() * RotationSpeed;
+        gameObject.transform.Rotate(0, turn, 0);
+    }
+}
