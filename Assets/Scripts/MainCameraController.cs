d1526d333a215bfaf345fcf4b15fd1e319b481c0
Justin Spahr-Summers <justin@jspahrsummers.com>
2020-08-02 21:43:41 +0100

    Main camera follows the player

---
+++ Assets/Scripts/MainCameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>Controls the main camera so that it follows an object across the scene.</summary>
+public sealed class MainCameraController : MonoBehaviour
+{
+    [Tooltip("The object that the camera should be following.")]
+    public GameObject followTarget;
+
+    void LateUpdate()
+    {
+        if (followTarget == null)
+        {
+            return;
+        }
+
+        // The camera does not move along the Z axis to follow, only horizontally and vertically relative to the viewport.
+        Vector3 followPosition = followTarget.transform.position;
+        transform.position = new Vector3(followPosition.x, followPosition.y, transform.position.z);
+    }
+}
