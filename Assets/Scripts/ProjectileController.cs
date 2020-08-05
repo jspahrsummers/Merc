3b18c33905dd882c4f4f83dcc7d01abd7a873107
Justin Spahr-Summers <justin@jspahrsummers.com>
2020-08-05 23:28:01 +0100

    Put a finite lifetime on blaster fire

---
+++ Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using Mirror;
+
+/// <summary>Manages the lifecycle of a projectile (e.g., a fired weapon).</summary>
+public sealed class ProjectileController : NetworkBehaviour
+{
+    [Tooltip("Number of seconds that this projectile should persist for, before disappearing.")]
+    public float lifetime = Mathf.Infinity;
+
+    public override void OnStartServer()
+    {
+        StartCoroutine(DisappearAfterLifetime());
+    }
+
+    [Server]
+    private IEnumerator DisappearAfterLifetime()
+    {
+        yield return new WaitForSeconds(lifetime);
+        Destroy(gameObject);
+    }
+}
