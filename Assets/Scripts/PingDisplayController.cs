92a9dc072a0b6f244d04c2f311e4d8e77cf1e287
Justin Spahr-Summers <justin@jspahrsummers.com>
2020-08-09 12:27:05 +0100

    Don't show ping when hosting
    It will always be 0.

---
+++ Assets/Scripts/PingDisplayController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Mirror;
+
+/// <summary>Enables or disables the ping display for a client.</summary>
+public sealed class PingDisplayController : MonoBehaviour
+{
+    void Start()
+    {
+        if (NetworkManager.singleton.mode != NetworkManagerMode.ClientOnly)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
