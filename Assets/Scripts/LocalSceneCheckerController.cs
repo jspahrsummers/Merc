aed6e2d3b2adb788a37aa4332c465e03be4d12a0
Justin Spahr-Summers <justin@jspahrsummers.com>
2020-08-11 10:55:04 +0100

    Hide local-only objects from the wrong scene

---
+++ Assets/Scripts/LocalSceneCheckerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>Used on <b>local only</b> objects to prevent them being shown when their scene is not active.</summary>
+/// <remarks>For client only operation, this is not really necessary; but when the client is also functioning as a server (i.e., "host mode," with all scenes loaded), this prevents the player from seeing objects they shouldn't.</remarks>
+public sealed class LocalSceneCheckerController : MonoBehaviour
+{
+    void Awake()
+    {
+        SceneManager.activeSceneChanged += ChangedActiveScene;
+    }
+
+    void OnEnable()
+    {
+        ChangedActiveScene(gameObject.scene, SceneManager.GetActiveScene());
+    }
+
+    void OnDestroy()
+    {
+        SceneManager.activeSceneChanged -= ChangedActiveScene;
+    }
+
+    private void ChangedActiveScene(Scene previous, Scene next)
+    {
+        gameObject.SetActive(gameObject.scene == next);
+    }
+}
