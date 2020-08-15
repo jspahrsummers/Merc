ecd07ef944c014f19c28c743219b1ea3501e7b34
Justin Spahr-Summers <justin@jspahrsummers.com>
2020-08-15 15:44:19 +0100

    Move scene management nonsense to GameController
    This is more within its purpose than the NetworkManager.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,7 +1,26 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Mirror;
+using MercExtensions;
+
+/// <summary>Sent from the client to the server when a scene has been loaded, but not yet activated, to allow the server to issue the next instruction.</summary>
+internal sealed class SceneReadyMessage : MessageBase
+{
+    /// <summary>The scene that was loaded.</summary>
+    public string sceneNameOrPath;
+}
+
+/// <summary>Sent from the server to the client, instructing it to activate a specific scene. The scene must have been loaded, but not already activated.</summary>
+internal sealed class ActivateSceneMessage : MessageBase
+{
+    /// <summary>The scene to activate.</summary>
+    public string sceneNameOrPath;
+}
+
+/// <summary>A list of players, to synchronize to clients.</summary>
+public sealed class SyncPlayerList : SyncSortedSet<string> { }
 
 /// <summary>Controls high-level game behaviors, including functionality that spans multiple scenes and players.</summary>
 /// <remarks>This class will also spawn a NetworkManager and start it in host mode if one does not exist yet. This is a convenience for dev-time testing, as usually the network manager is created on the main menu before the GameController is instantiated.</remarks>
@@ -10,14 +29,17 @@ public sealed class GameController : NetworkBehaviour
     [Tooltip("Prefab for spawning the network manager, if it does not already exist.")]
     public MercNetworkManager networkManagerPrefab;
 
+    [Scene, Tooltip("Scene to additively load on the client, and move their player object to, after connection and initial load of the online scene.")]
+    public string firstAdditiveScene;
+
     /// <summary>The active network manager.</summary>
     private MercNetworkManager networkManager;
 
-    /// <summary>A list of players, to synchronize to clients.</summary>
-    public sealed class SyncPlayerList : SyncSortedSet<string> { }
-
     /// <summary>All players currently connected to this server.</summary>
     public SyncPlayerList onlinePlayerList = new SyncPlayerList();
+
+    /// <summary>On the client, tracks an outstanding scene load operation that the server has not let complete yet.</summary>
+    private AsyncOperation clientSceneLoadOperation;
 
     public static GameController Find()
     {
@@ -41,6 +63,9 @@ public sealed class GameController : NetworkBehaviour
 
     public override void OnStartServer()
     {
+        base.OnStartServer();
+        NetworkServer.RegisterHandler<SceneReadyMessage>(ClientSceneReady);
+
         foreach (var nickname in networkManager.networkAuthenticator.nicknames.Values)
         {
             onlinePlayerList.Add(nickname);
@@ -48,8 +73,17 @@ public sealed class GameController : NetworkBehaviour
 
         networkManager.clientConnectedToServer.AddListener(ClientConnectedToServer);
         networkManager.clientDisconnectedFromServer.AddListener(ClientDisconnectedFromServer);
+        networkManager.serverAddedPlayer.AddListener(ServerAddedPlayer);
 
-        LoadAllScenes();
+        StartCoroutine(LoadAllScenes());
+    }
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        NetworkClient.RegisterHandler<ActivateSceneMessage>(ActivateScene);
+
+        networkManager.clientChangeScene.AddListener(ClientChangeScene);
     }
 
     void OnDestroy()
@@ -59,7 +93,7 @@ public sealed class GameController : NetworkBehaviour
     }
 
     [Server]
-    private void LoadAllScenes()
+    private IEnumerator LoadAllScenes()
     {
         var loadedScenes = new HashSet<int>();
         for (int i = 0; i < SceneManager.sceneCount; i++)
@@ -76,7 +110,7 @@ public sealed class GameController : NetworkBehaviour
             }
 
             Debug.Log($"Preloading scene at build index {i}");
-            SceneManager.LoadSceneAsync(i, LoadSceneMode.Additive);
+            yield return SceneManager.LoadSceneAsync(i, LoadSceneMode.Additive);
         }
     }
 
@@ -96,5 +130,148 @@ public sealed class GameController : NetworkBehaviour
         }
 
         onlinePlayerList.Remove(nickname);
+    }
+
+    [Server]
+    private void ClientSceneReady(NetworkConnection connection, SceneReadyMessage message)
+    {
+        Scene? scene = SceneManagerExtensions.GetSceneByPathOrName(message.sceneNameOrPath);
+        if (scene == null)
+        {
+            Debug.LogWarning($"Scene {message.sceneNameOrPath} not loaded on server despite client readying it");
+            return;
+        }
+
+        Debug.Log($"Moving {connection} to scene {message.sceneNameOrPath}");
+        SceneManager.MoveGameObjectToScene(connection.identity.gameObject, scene.Value);
+        connection.Send(new ActivateSceneMessage { sceneNameOrPath = message.sceneNameOrPath });
+    }
+
+    [Server]
+    private void ServerAddedPlayer(NetworkConnection connection)
+    {
+        string scenePath = firstAdditiveScene;
+        Debug.Log($"Asking player {connection} to load {scenePath}");
+
+        // In host mode, if this is the local player, special case our activation path as the scene may already have been loaded.
+        if (connection.identity.isLocalPlayer)
+        {
+            StartCoroutine(WaitForSceneToLoadThenMovePlayer(scenePath, connection.identity.gameObject));
+        }
+        else
+        {
+            connection.Send(new SceneMessage { sceneName = scenePath, sceneOperation = SceneOperation.LoadAdditive, customHandling = true });
+        }
+    }
+
+    [Server]
+    private IEnumerator WaitForSceneToLoadThenMovePlayer(string sceneNameOrPath, GameObject player)
+    {
+        // This scene should be loaded by GameController already.
+        Scene? scene;
+        do
+        {
+            yield return null;
+            scene = SceneManagerExtensions.GetSceneByPathOrName(sceneNameOrPath);
+        } while (scene == null || !scene.Value.isLoaded);
+
+        SceneManager.SetActiveScene(scene.Value);
+        SceneManager.MoveGameObjectToScene(player, scene.Value);
+        Debug.Log($"Moved host player to {sceneNameOrPath}");
+    }
+
+    [Client]
+    private void ClientChangeScene(string sceneNameOrPath, SceneOperation sceneOperation)
+    {
+        if (clientSceneLoadOperation != null)
+        {
+            Debug.LogWarning($"Scene load operation already in progress!");
+        }
+
+        switch (sceneOperation)
+        {
+            case SceneOperation.Normal:
+                clientSceneLoadOperation = SceneManager.LoadSceneAsync(sceneNameOrPath);
+                break;
+
+            case SceneOperation.LoadAdditive:
+                if (SceneManagerExtensions.GetSceneByPathOrName(sceneNameOrPath) == null)
+                {
+                    Debug.Log($"Starting to load scene {sceneNameOrPath}");
+                    clientSceneLoadOperation = SceneManager.LoadSceneAsync(sceneNameOrPath, LoadSceneMode.Additive);
+                }
+                else
+                {
+                    Debug.Log($"Scene {sceneNameOrPath} already loaded, skipping");
+                }
+
+                break;
+
+            case SceneOperation.UnloadAdditive:
+                if (SceneManagerExtensions.GetSceneByPathOrName(sceneNameOrPath) != null)
+                {
+                    Debug.Log($"Starting to unload scene {sceneNameOrPath}");
+                    clientSceneLoadOperation = SceneManager.UnloadSceneAsync(sceneNameOrPath, UnloadSceneOptions.UnloadAllEmbeddedSceneObjects);
+                    clientSceneLoadOperation.completed += op => clientSceneLoadOperation = null;
+                }
+                else
+                {
+                    Debug.Log($"Scene {sceneNameOrPath} already unloaded, skipping");
+                }
+
+                // The rest of the logic is really only applicable to loading.
+                return;
+        }
+
+        if (clientSceneLoadOperation != null)
+        {
+            clientSceneLoadOperation.allowSceneActivation = false;
+        }
+
+        StartCoroutine(NotifyServerWhenSceneReady(sceneNameOrPath));
+
+    }
+
+    [Client]
+    private IEnumerator NotifyServerWhenSceneReady(string sceneNameOrPath)
+    {
+        if (clientSceneLoadOperation != null)
+        {
+            while (clientSceneLoadOperation.progress < 0.9f)
+            {
+                yield return null;
+            }
+        }
+
+        Debug.Log($"Prepared scene {sceneNameOrPath}, notifying server");
+        NetworkClient.Send(new SceneReadyMessage { sceneNameOrPath = sceneNameOrPath });
+    }
+
+    [Client]
+    private void ActivateScene(NetworkConnection connection, ActivateSceneMessage message)
+    {
+        if (clientSceneLoadOperation == null)
+        {
+            Debug.LogWarning($"No in-progress scene load for {message.sceneNameOrPath}");
+        }
+
+        Debug.Log($"Activating {message.sceneNameOrPath}");
+        StartCoroutine(WaitForSceneActivationThenSetActive(message.sceneNameOrPath));
+    }
+
+    [Client]
+    private IEnumerator WaitForSceneActivationThenSetActive(string sceneNameOrPath)
+    {
+        if (clientSceneLoadOperation != null)
+        {
+            yield return clientSceneLoadOperation;
+            clientSceneLoadOperation = null;
+        }
+
+        Scene scene = SceneManagerExtensions.GetSceneByPathOrName(sceneNameOrPath).Value;
+        if (!SceneManager.SetActiveScene(scene))
+        {
+            Debug.LogWarning($"Failed to activate scene {sceneNameOrPath}");
+        }
     }
 }
--- Assets/Scripts/MercNetworkManager.cs
+++ Assets/Scripts/MercNetworkManager.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 using Mirror;
-using MercExtensions;
+
+/// <summary>An event tagged with a network connection that it happened to/on.</summary>
+public sealed class NetworkConnectionEvent : UnityEvent<NetworkConnection> { }
+
+/// <summary>An event requesting or completing a change to the named scene, described by the provided operation.</summary>
+public sealed class SceneChangeEvent : UnityEvent<string, SceneOperation> { }
 
 /// <summary>Custom NetworkManager type that supports listeners for the default "callbacks."</summary>
 public sealed class MercNetworkManager : NetworkManager
 {
-    [Scene, Tooltip("Scene to additively load on the client, and move their player object to, after connection and initial load of the online scene.")]
-    public string firstAdditiveScene;
-
     [Tooltip("The authenticator to use for connections via this network manager.")]
     public MercNetworkAuthenticator networkAuthenticator;
 
-    /// <summary>An event tagged with a network connection that it happened to/on.</summary>
-    public sealed class NetworkConnectionEvent : UnityEvent<NetworkConnection> { }
-
     [Tooltip("Event invoked when this client successfully connects to a server.")]
     public UnityEvent clientConnected = new UnityEvent();
+
+    [Tooltip("Event invoked when this client is requested to change scenes using custom handling.")]
+    public SceneChangeEvent clientChangeScene = new SceneChangeEvent();
 
     [Tooltip("Event invoked when this client encounters a network error.")]
     public UnityEvent clientError = new UnityEvent();
@@ -33,82 +35,12 @@ public sealed class MercNetworkManager : NetworkManager
     [Tooltip("Event invoked when a client disconnects from this server.")]
     public NetworkConnectionEvent clientDisconnectedFromServer = new NetworkConnectionEvent();
 
-    /// <summary>Sent from the client to the server when a scene has been loaded, but not yet activated, to allow the server to issue the next instruction.</summary>
-    private sealed class SceneReadyMessage : MessageBase
-    {
-        /// <summary>The scene that was loaded.</summary>
-        public string sceneNameOrPath;
-    }
-
-    /// <summary>Sent from the server to the client, instructing it to activate a specific scene. The scene must have been loaded, but not already activated.</summary>
-    private sealed class ActivateSceneMessage : MessageBase
-    {
-        /// <summary>The scene to activate.</summary>
-        public string sceneNameOrPath;
-    }
-
-    /// <summary>On the client, tracks all outstanding scene load operations that the server has not let complete yet (keyed by sceneNameOrPath).</summary>
-    private Dictionary<string, AsyncOperation> sceneLoadOperations = new Dictionary<string, AsyncOperation>();
+    [Tooltip("Event invoked when the server has added a player object corresponding to a client.")]
+    public NetworkConnectionEvent serverAddedPlayer = new NetworkConnectionEvent();
 
     public static MercNetworkManager Find()
     {
         return (MercNetworkManager)NetworkManager.singleton;
-    }
-
-    public override void OnStartServer()
-    {
-        base.OnStartServer();
-        NetworkServer.RegisterHandler<SceneReadyMessage>(ClientSceneReady);
-    }
-
-    public override void OnStartClient()
-    {
-        base.OnStartClient();
-        NetworkClient.RegisterHandler<ActivateSceneMessage>(ActivateScene);
-    }
-
-    private void ClientSceneReady(NetworkConnection connection, SceneReadyMessage message)
-    {
-        Scene? scene = SceneManagerExtensions.GetSceneByPathOrName(message.sceneNameOrPath);
-        if (scene == null)
-        {
-            Debug.LogWarning($"Scene {message.sceneNameOrPath} not loaded on server despite client readying it");
-            return;
-        }
-
-        Debug.Log($"Moving {connection} to scene {message.sceneNameOrPath}");
-        SceneManager.MoveGameObjectToScene(connection.identity.gameObject, scene.Value);
-        connection.Send(new ActivateSceneMessage { sceneNameOrPath = message.sceneNameOrPath });
-    }
-
-    private void ActivateScene(NetworkConnection connection, ActivateSceneMessage message)
-    {
-        AsyncOperation operation;
-        if (sceneLoadOperations.TryGetValue(message.sceneNameOrPath, out operation))
-        {
-            operation.allowSceneActivation = true;
-        }
-        else
-        {
-            Debug.LogWarning($"No in-progress scene load for {message.sceneNameOrPath}");
-        }
-
-        Debug.Log($"Activating {message.sceneNameOrPath}");
-        StartCoroutine(WaitForSceneActivationThenSetActive(operation, message.sceneNameOrPath));
-    }
-
-    private IEnumerator WaitForSceneActivationThenSetActive(AsyncOperation operation, string sceneNameOrPath)
-    {
-        if (operation != null)
-        {
-            yield return operation;
-        }
-
-        Scene scene = SceneManagerExtensions.GetSceneByPathOrName(sceneNameOrPath).Value;
-        if (!SceneManager.SetActiveScene(scene))
-        {
-            Debug.LogWarning($"Failed to activate scene {sceneNameOrPath}");
-        }
     }
 
     // Invoked on the client after authenticating successfully to the server.
@@ -120,78 +52,14 @@ public sealed class MercNetworkManager : NetworkManager
 
     public override void OnClientChangeScene(string sceneNameOrPath, SceneOperation sceneOperation, bool customHandling)
     {
-        if (!customHandling)
+        if (customHandling)
+        {
+            clientChangeScene.Invoke(sceneNameOrPath, sceneOperation);
+        }
+        else
         {
             base.OnClientChangeScene(sceneNameOrPath, sceneOperation, customHandling);
-            return;
         }
-
-        if (sceneLoadOperations.ContainsKey(sceneNameOrPath))
-        {
-            Debug.LogWarning($"Scene change operation for {sceneNameOrPath} already in progress, skipping");
-            return;
-        }
-
-        AsyncOperation asyncOperation = null;
-
-        switch (sceneOperation)
-        {
-            case SceneOperation.Normal:
-                asyncOperation = SceneManager.LoadSceneAsync(sceneNameOrPath);
-                break;
-
-            case SceneOperation.LoadAdditive:
-                if (SceneManagerExtensions.GetSceneByPathOrName(sceneNameOrPath) == null)
-                {
-                    Debug.Log($"Starting to load scene {sceneNameOrPath}");
-                    asyncOperation = SceneManager.LoadSceneAsync(sceneNameOrPath, LoadSceneMode.Additive);
-                }
-                else
-                {
-                    Debug.Log($"Scene {sceneNameOrPath} already loaded, skipping");
-                }
-
-                break;
-
-            case SceneOperation.UnloadAdditive:
-                if (SceneManagerExtensions.GetSceneByPathOrName(sceneNameOrPath) != null)
-                {
-                    Debug.Log($"Starting to unload scene {sceneNameOrPath}");
-                    asyncOperation = SceneManager.UnloadSceneAsync(sceneNameOrPath, UnloadSceneOptions.UnloadAllEmbeddedSceneObjects);
-                }
-                else
-                {
-                    Debug.Log($"Scene {sceneNameOrPath} already unloaded, skipping");
-                }
-
-                break;
-        }
-
-        if (asyncOperation != null)
-        {
-            asyncOperation.allowSceneActivation = false;
-            sceneLoadOperations[sceneNameOrPath] = asyncOperation;
-            asyncOperation.completed += op => sceneLoadOperations.Remove(sceneNameOrPath);
-        }
-
-        if (sceneOperation != SceneOperation.UnloadAdditive)
-        {
-            StartCoroutine(NotifyServerWhenSceneReady(sceneNameOrPath, asyncOperation));
-        }
-    }
-
-    private IEnumerator NotifyServerWhenSceneReady(string sceneNameOrPath, AsyncOperation operation)
-    {
-        if (operation != null)
-        {
-            while (operation.progress < 0.9f)
-            {
-                yield return null;
-            }
-        }
-
-        Debug.Log($"Prepared scene {sceneNameOrPath}, notifying server");
-        NetworkClient.Send(new SceneReadyMessage { sceneNameOrPath = sceneNameOrPath });
     }
 
     public override void OnClientError(NetworkConnection connection, int errorCode)
@@ -209,34 +77,7 @@ public sealed class MercNetworkManager : NetworkManager
     public override void OnServerAddPlayer(NetworkConnection connection)
     {
         base.OnServerAddPlayer(connection);
-
-        string scenePath = firstAdditiveScene;
-        Debug.Log($"Asking player {connection} to load {scenePath}");
-
-        // In host mode, if this is the local player, special case our activation path as the scene may already have been loaded.
-        if (connection.identity.isLocalPlayer)
-        {
-            StartCoroutine(WaitForSceneToLoadThenMovePlayer(scenePath, connection.identity.gameObject));
-        }
-        else
-        {
-            connection.Send(new SceneMessage { sceneName = scenePath, sceneOperation = SceneOperation.LoadAdditive, customHandling = true });
-        }
-    }
-
-    private IEnumerator WaitForSceneToLoadThenMovePlayer(string sceneNameOrPath, GameObject player)
-    {
-        // This scene should be loaded by GameController already.
-        Scene? scene;
-        do
-        {
-            yield return null;
-            scene = SceneManagerExtensions.GetSceneByPathOrName(sceneNameOrPath);
-        } while (scene == null || !scene.Value.isLoaded);
-
-        SceneManager.SetActiveScene(scene.Value);
-        SceneManager.MoveGameObjectToScene(player, scene.Value);
-        Debug.Log($"Moved host player to {sceneNameOrPath}");
+        serverAddedPlayer.Invoke(connection);
     }
 
     public override void OnServerError(NetworkConnection connection, int errorCode)
