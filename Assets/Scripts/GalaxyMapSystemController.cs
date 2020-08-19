164f77e7518334c2df169c3840b6ec4415da55a1
Justin Spahr-Summers <justin@jspahrsummers.com>
2020-08-19 16:00:47 +0100

    Implement galaxy map selection state

--- Assets/Scripts/GalaxyMapSystemController.cs
+++ Assets/Scripts/GalaxyMapSystemController.cs
@@ -1,10 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 /// <summary>Displays a single system on the galaxy map screen, and handles UI behaviors related to it.</summary>
 public sealed class GalaxyMapSystemController : MonoBehaviour
 {
+    [Tooltip("The text element rendering the name of this system.")]
+    public TMP_Text nameText;
+
+    [Tooltip("The map icon for this system.")]
+    public Image icon;
+
     public void SetSelected(bool selected)
     {
-        Debug.Log($"{gameObject.name} selected: {selected}");
+        var color = selected ? Color.yellow : Color.white;
+        icon.color = color;
+        nameText.color = color;
     }
 }
