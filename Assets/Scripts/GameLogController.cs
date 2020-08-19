887f601b3fbd194bcdd7d8f25d211e92d944e1e3
Justin Spahr-Summers <justin@jspahrsummers.com>
2020-08-19 16:45:37 +0100

    Add a simple scrolling game log

---
+++ Assets/Scripts/GameLogController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+/// <summary>Presents scrolling log messages to the user.</summary>
+public sealed class GameLogController : MonoBehaviour
+{
+    [Tooltip("The text of the log.")]
+    public TMP_Text logText;
+
+    [Tooltip("The scrolling view containing the log.")]
+    public ScrollRect scrollRect;
+
+    /// <summary>Y velocity at which to automatically scroll the view when a new message appears.</summary>
+    const float NewMessageScrollVelocity = 100f;
+
+    void Start()
+    {
+        logText.text = "";
+        StartCoroutine(LogRepeatedly());
+    }
+
+    private IEnumerator LogRepeatedly()
+    {
+        for (int i = 0; ; i++)
+        {
+            yield return new WaitForSeconds(0.5f);
+            AddMessage($"Log message {i}");
+        }
+    }
+
+    /// <summary>Adds a new message to the tip of the log.</summary>
+    public void AddMessage(string message)
+    {
+        if (logText.text.Length > 0)
+        {
+            message = "\n" + message;
+        }
+
+        logText.text += message;
+        scrollRect.velocity = new Vector2(0, NewMessageScrollVelocity);
+    }
+}
