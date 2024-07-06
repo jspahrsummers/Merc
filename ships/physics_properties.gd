fae3234c63f90a2da0cbb7435c31c668b7b28a23
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-07-06 22:20:39 +0100

    Replace existing ship prefabs

--- ships/physics_properties.gd
+++
@@ -1,6 +0,0 @@
-extends Resource
-class_name PhysicsProperties
-
-## Defines values to initialize a physics body, like a [RigidBody3D], with.
-
-@export var mass: float = 1.0
--- ships/ship.gd
+++ ships/ship.gd
@@ -1,45 +1,11 @@
-extends Resource
-class_name Ship
+extends RigidBody3D
 
-## Represents a ship's physics and gameplay features.
-
-@export var physics_properties: PhysicsProperties
-@export var hull: Hull
-@export var shield: Shield
-@export var thruster: Thruster
-@export var spin_thruster: SpinThruster
-@export var battery: Battery
-@export var power_generator: PowerGenerator
-@export var weapons: Array[Weapon]
-
-## When arriving from a hyperspace jump, the ship's position will be randomized around the system center. This is the maximum radius (in m) that the randomized position is allowed to occupy.
-# TODO: Move this somewhere better.
-@export var hyperspace_arrival_radius: float
-
-func _get_property_list() -> Array[Dictionary]:
-    return [
-        {
-            "name": "hull",
-            "class_name": &"Hull",
-            "type": TYPE_OBJECT,
-            "hint": PROPERTY_HINT_NONE,
-            "hint_stages": "",
-            "usage": PROPERTY_USAGE_SCRIPT_VARIABLE|PROPERTY_USAGE_ALWAYS_DUPLICATE
-        },
-        {
-            "name": "shield",
-            "class_name": &"Shield",
-            "type": TYPE_OBJECT,
-            "hint": PROPERTY_HINT_NONE,
-            "hint_stages": "",
-            "usage": PROPERTY_USAGE_SCRIPT_VARIABLE|PROPERTY_USAGE_ALWAYS_DUPLICATE
-        },
-        {
-            "name": "battery",
-            "class_name": &"Battery",
-            "type": TYPE_OBJECT,
-            "hint": PROPERTY_HINT_NONE,
-            "hint_stages": "",
-            "usage": PROPERTY_USAGE_SCRIPT_VARIABLE|PROPERTY_USAGE_ALWAYS_DUPLICATE
-        },
-    ]
+@onready var combat_object: CombatObject = $CombatObject
+@onready var targeting_system: TargetingSystem = $TargetingSystem
+@onready var rigid_body_thruster: RigidBodyThruster = $RigidBodyThruster
+@onready var rigid_body_direction: RigidBodyDirection = $RigidBodyDirection
+@onready var rigid_body_turner: RigidBodyTurner = $RigidBodyTurner # optional
+@onready var power_management_unit: PowerManagementUnit = $PowerManagementUnit
+@onready var radar_object: RadarObject = $RadarObject
+@onready var weapon_mounts: Array[WeaponMount] = self.get_children().filter(func(node: Node) -> bool: return node is WeaponMount)
+@onready var shield_recharger: ShieldRecharger = $ShieldRecharger # optional
