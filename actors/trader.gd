e441f593563ab75fd61c589bf4e43cf99cf4950a
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-07-07 16:41:55 +0100

    Dumb, basically broken, trader AI

---
+++ actors/trader.gd
@@ -0,0 +1,62 @@
+extends Node3D
+class_name Trader
+
+## AI for a ship that behaves like a trader, avoiding combat.
+##
+## [b]This script expects the parent node to be a [Ship].[/b]
+
+# TODO: Deduplicate code with Pirate class.
+
+## For thrusting, the tolerance (in degrees) for being slightly off-rotated.
+@export var direction_tolerance_deg: float = 10.0
+
+## The tolerance (in m) for hitting the destination point, before selecting a new one.
+@export var destination_tolerance: float = 1.0
+
+@onready var _ship := self.get_parent() as Ship
+var _direction_tolerance_rad: float
+var _destination: Node3D = null
+
+func _ready() -> void:
+    self._direction_tolerance_rad = deg_to_rad(self.direction_tolerance_deg)
+
+func _select_new_destination() -> void:
+    var planets: Array[Node3D] = []
+    planets.assign(self.get_tree().get_nodes_in_group("planets"))
+    if self._destination:
+        planets.erase(self._destination)
+    
+    self._destination = planets.pick_random() if planets else null
+
+func _desired_direction() -> Vector3:
+    if not self._destination:
+        return Vector3.ZERO
+
+    return (self._destination.global_position - self._ship.global_position).normalized()
+
+func _pointing_in_direction(direction: Vector3) -> bool:
+    var current_direction := - self._ship.global_transform.basis.z
+    return current_direction.angle_to(direction) <= self._direction_tolerance_rad
+
+func _distance_to_destination() -> float:
+    if not self._destination:
+        return 0.0
+
+    return self._ship.global_position.distance_to(self._destination.global_position)
+
+func _physics_process(_delta: float) -> void:
+    if not self._destination:
+        self._select_new_destination()
+
+    var desired_direction := self._desired_direction()
+    self._ship.rigid_body_direction.direction = desired_direction
+
+    if not self._pointing_in_direction(desired_direction):
+        self._ship.rigid_body_thruster.throttle = 0.0
+        return
+    
+    if self._distance_to_destination() > self.destination_tolerance:
+        self._ship.rigid_body_thruster.throttle = 1.0
+    else:
+        self._ship.rigid_body_thruster.throttle = 0.0
+        self._select_new_destination()
