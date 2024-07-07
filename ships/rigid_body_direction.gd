7c06c4bbd60a8bea71fe8cc156e1ff36086e6459
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-07-07 12:59:04 +0100

    Simplify `RigidBodyDirection` logic, with minor caveat

--- ships/rigid_body_direction.gd
+++ ships/rigid_body_direction.gd
@@ -15,29 +15,27 @@ class_name RigidBodyDirection
 @export var direction: Vector3 = Vector3.ZERO:
     set(value):
         direction = value
-        if not direction.is_zero_approx():
-            self._desired_basis = Basis.looking_at(direction)
 
 ## The [RigidBody3D] to rotate.
 @onready var _rigid_body := get_parent() as RigidBody3D
-
-var _desired_basis := Basis.IDENTITY
 
 func _physics_process(delta: float) -> void:
     if is_zero_approx(self.battery.power) or self.direction.is_zero_approx():
         return
     
+    var desired_basis := Basis.looking_at(self.direction)
+    if self._rigid_body.global_basis.is_equal_approx(desired_basis):
+        return
+    
     if not self._rigid_body.angular_velocity.is_zero_approx():
         self._rigid_body.angular_velocity = Vector3.ZERO
-    
-    var angle_delta := self._rigid_body.global_basis.get_rotation_quaternion().angle_to(self._desired_basis.get_rotation_quaternion())
-    var max_turn := self.spin_thruster.turning_rate * delta
-    var desired_turn := minf(max_turn, angle_delta) / max_turn
-    var slerp_weight := desired_turn / max_turn
 
-    var desired_power := self.spin_thruster.power_consumption_rate * slerp_weight
+    # This may consume more power than necessary, if less than a full rotation
+    # step is still needed. This is an acceptable cost for the simplicity of
+    # this function, also taking into account that the error will be bounded by
+    # `delta`.
+    var desired_power := self.spin_thruster.power_consumption_rate * delta
     var consumed_power := self.battery.consume_up_to(desired_power)
     var consumed_percentage := consumed_power / desired_power
 
-    slerp_weight *= consumed_percentage
-    self._rigid_body.global_basis = self._rigid_body.global_basis.slerp(self._desired_basis, slerp_weight)
+    self._rigid_body.global_basis = self._rigid_body.global_basis.slerp(desired_basis, self.spin_thruster.turning_rate * delta * consumed_percentage)
