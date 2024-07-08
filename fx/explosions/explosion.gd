9718c1e29361f58583f171b2088f3cc9bd6c1492
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-07-08 22:13:13 +0100

    No need to manually `play` explosion sprites

--- fx/explosions/explosion.gd
+++ fx/explosions/explosion.gd
@@ -1,14 +1,17 @@
 extends AnimatedSprite3D
 
-## An audio clip to play when this object enters the scene.
+## An explosion effect.
+##
+## This sprite should have autoplay enabled.
+
+## An audio clip that will play when this object enters the scene.
+##
+## This clip must be set to autoplay.
 @export var audio: AudioStreamPlayer3D
 
 func _ready() -> void:
     self.animation_finished.connect(_on_animation_finished)
     self.audio.finished.connect(_on_audio_finished)
-
-func _enter_tree() -> void:
-    self.play()
 
 func _on_animation_finished() -> void:
     if !self.audio.playing:
