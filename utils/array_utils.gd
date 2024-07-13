2376325417b6a231c157b847444547f5ee0704d7
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-07-14 00:06:07 +0100

    Abstract out array cycling logic
    @skip-notify

---
+++ utils/array_utils.gd
@@ -0,0 +1,14 @@
+extends Node
+
+## Finds the next object in [param array] after [param starting_from], wrapping around to `null` if the end of the array is reached.
+func cycle_through(array: Array, starting_from: Variant) -> Variant:
+    if array.size() == 0:
+        return null
+    
+    if starting_from == null:
+        return array[0]
+    
+    var index := array.find(starting_from)
+    assert(index >= 0, "Cannot find starting_from in array")
+
+    return array[index + 1] if index + 1 < array.size() else null
