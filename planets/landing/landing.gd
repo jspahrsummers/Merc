7bb832bfa4db3f7facaed54bffdd632180ba5d1f
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-07-13 13:19:42 +0100

    Stub script for landing screen

---
+++ planets/landing/landing.gd
@@ -0,0 +1,22 @@
+extends Window
+
+func _on_bar_button_pressed() -> void:
+    pass # Replace with function body.
+
+func _on_trading_button_pressed() -> void:
+    pass # Replace with function body.
+
+func _on_missions_button_pressed() -> void:
+    pass # Replace with function body.
+
+func _on_outfitter_button_pressed() -> void:
+    pass # Replace with function body.
+
+func _on_shipyard_button_pressed() -> void:
+    pass # Replace with function body.
+
+func _on_refuel_button_pressed() -> void:
+    pass # Replace with function body.
+
+func _on_depart_button_pressed() -> void:
+    self.visible = false
