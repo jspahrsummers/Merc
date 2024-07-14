06469ebd7f8865530dfc8da2b685f8307ed5d4a3
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-07-14 21:34:06 +0100

    `FuelBar` UI and styling

--- screens/game/hud/fuel_bar.gd
+++ screens/game/hud/fuel_bar.gd
@@ -3,18 +3,18 @@ extends Control
 @export var background: NinePatchRect
 @export var fill: NinePatchRect
 
-const PIP_WIDTH = 8
+const PIP_WIDTH = 10
 
 var max_fuel: float:
     set(value):
         max_fuel = value
-        self.background.size.x = self.max_fuel * PIP_WIDTH
+        self.background.custom_minimum_size.x = self.max_fuel * PIP_WIDTH
 
 var fuel: float:
     set(value):
         fuel = value
         self.fill.size.x = self.fuel * PIP_WIDTH
 
-func _ready() -> void:
-    self.max_fuel = 11.5
-    self.fuel = 5.2
+func _on_player_hyperdrive_changed(_player: Player, hyperdrive: Hyperdrive) -> void:
+    self.max_fuel = hyperdrive.max_fuel
+    self.fuel = hyperdrive.fuel
