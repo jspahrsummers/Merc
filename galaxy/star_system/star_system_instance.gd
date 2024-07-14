f79aa7210ea575a7ffded94c1dfcd4a0eec6754f
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-07-14 19:44:18 +0100

    `star_system_instance_for_node()`

--- galaxy/star_system/star_system_instance.gd
+++ galaxy/star_system/star_system_instance.gd
@@ -1,5 +1,15 @@
 extends Node3D
+class_name StarSystemInstance
 
 ## Instantiates a star system.
 
 @export var star_system: StarSystem
+
+## Walks the ancestors of [param node] and finds the first [StarSystemInstance] (i.e., the one containing the node).
+static func star_system_instance_for_node(node: Node) -> StarSystemInstance:
+    while not (node is StarSystemInstance):
+        node = node.get_parent()
+        if node == null:
+            return null
+    
+    return node
