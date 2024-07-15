0ed180ad852ac7f6ce6a914766d78f71c637af22
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-07-15 20:07:57 +0100

    Replace `ArrayUtils` and `MathUtils` autoloads with static functions
    Resolves #28.

--- utils/math_utils.gd
+++ utils/math_utils.gd
@@ -1,4 +1,4 @@
-extends Node
+class_name MathUtils
 
 ## Microseconds per second.
 const USEC_PER_SEC = 1000000
@@ -9,6 +9,6 @@ const USEC_PER_SEC_F = USEC_PER_SEC as float
 ## Returns a unit vector pointing in a random (2D) angle.
 ##
 ## This vector can be multiplied with a desired radius to get a random position within a circle.
-func random_unit_vector() -> Vector3:
+static func random_unit_vector() -> Vector3:
     var angle := randf_range(0, 2 * PI)
     return Vector3(cos(angle), 0, sin(angle))
