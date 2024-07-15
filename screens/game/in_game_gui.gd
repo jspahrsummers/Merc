b03264a5dd13bb708033f11514bbdb03cf1639ba
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-07-15 20:16:57 +0100

    Instantiate game over dialog on demand

--- screens/game/in_game_gui.gd
+++ screens/game/in_game_gui.gd
@@ -1,7 +1,13 @@
 extends CanvasLayer
 
 @export var galaxy_map_window: Window
+@export var game_over_scene: PackedScene
 @export var exit_dialog: Window
+
+func _on_player_ship_destroyed(_player: Player) -> void:
+    var game_over_window: Window = game_over_scene.instantiate()
+    self.add_child(game_over_window)
+    game_over_window.show()
 
 func _unhandled_input(event: InputEvent) -> void:
     if event.is_action_pressed("toggle_galaxy_map"):
