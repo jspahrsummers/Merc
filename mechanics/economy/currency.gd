58732400b67ac948dc7990c4cdcdd577d5da9984
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-07-20 17:05:47 +0100

    Add optional mass and volume for currencies too

--- mechanics/economy/currency.gd
+++ mechanics/economy/currency.gd
@@ -6,5 +6,13 @@ class_name Currency
 ## The human-readable name for this currency.
 @export var name: String
 
+## The [b]in-game mass[/b] per one unit of this item, in kg.
+##
+## This is added to ship mass when the asset is being carried.
+@export var mass: float = 0.0
+
+## The volume of one unit of this item, in liters.
+@export var volume: float = 0.0
+
 func _to_string() -> String:
     return "Currency:" + self.name
