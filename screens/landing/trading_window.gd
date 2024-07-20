3941087a65490551d4183c1beb984d75f560748c
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-07-20 22:57:42 +0100

    WIP trading window

--- screens/landing/trading_window.gd
+++ screens/landing/trading_window.gd
@@ -12,11 +12,17 @@ func _ready() -> void:
     self.tab_container.tabs_visible = self.market.currencies.size() > 0 and self.market.commodities.size() > 0
 
     for commodity: Commodity in self.market.commodities:
+        var price: int = self.market.commodities.get(commodity, 0)
+
         var trade_cell: TradeCell = self.trade_cell_scene.instantiate()
-        trade_cell.label.text = commodity.name
+        trade_cell.asset_label.text = commodity.name
+        trade_cell.price_label.text = "x %s credits" % (float(price) / Currency.GRANULARITY)
         self.commodities_container.add_child(trade_cell)
 
     for currency: Currency in self.market.currencies:
+        var price: int = self.market.currencies.get(currency, 0)
+
         var trade_cell: TradeCell = self.trade_cell_scene.instantiate()
-        trade_cell.label.text = currency.name
+        trade_cell.asset_label.text = currency.name
+        trade_cell.price_label.text = "x %s credits" % (float(price) / Currency.GRANULARITY)
         self.currencies_container.add_child(trade_cell)
