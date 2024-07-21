234153f29e61c24971b0a333b26571fd84e6da24
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-07-21 12:25:54 +0100

    Helper methods on `TradeAsset` for uniform withdrawal from cargo or bank

--- mechanics/economy/currency.gd
+++ mechanics/economy/currency.gd
@@ -3,5 +3,11 @@ class_name Currency
 
 ## Represents a digital currency that can be used to pay for goods and services.
 
+func take_up_to(amount: int, _cargo_hold: CargoHold, bank_account: BankAccount) -> int:
+    return bank_account.withdraw_up_to(self, amount)
+
+func take_exactly(amount: int, _cargo_hold: CargoHold, bank_account: BankAccount, allow_negative: bool=false) -> bool:
+    return bank_account.withdraw_exactly(self, amount, allow_negative)
+
 func _to_string() -> String:
     return "Currency:" + self.name
--- mechanics/economy/trade_asset.gd
+++ mechanics/economy/trade_asset.gd
@@ -11,5 +11,19 @@ class_name TradeAsset
 ## This allows us to use [int]s for computing fractional amounts of this asset, without the precision loss of floating-point math.
 @export var granularity: int = 1
 
+## Takes up to the specified amount of this asset from a [CargoHold] or [BankAccount], whichever is appropriate.
+##
+## Returns the amount actually taken, which may be less than the requested if there's not enough.
+func take_up_to(_amount: int, _cargo_hold: CargoHold, _bank_account: BankAccount) -> int:
+    assert(false, "Meant to be implemented by subclasses")
+    return 0
+
+## Takes exactly the specified amount of this asset from a [CargoHold] or [BankAccount], whichever is appropriate.
+##
+## If [param allow_negative] is set [b]and[/b] this [TradeAsset] is a [Currency], the account can go into negative balance; otherwise, an attempt to take more than what's available will fail and return false.
+func take_exactly(_amount: int, _cargo_hold: CargoHold, _bank_account: BankAccount, _allow_negative: bool=false) -> bool:
+    assert(false, "Meant to be implemented by subclasses")
+    return false
+
 func _to_string() -> String:
     return "TradeAsset:" + self.name
