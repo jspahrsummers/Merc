2e6abc800487f396a247e0be49b83b473793ef92
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-07-21 11:47:22 +0100

    `RigidBodyCargo`

---
+++ mechanics/physics/rigid_body_cargo.gd
@@ -0,0 +1,24 @@
+extends Node3D
+class_name RigidBodyCargo
+
+## Attaches to a [RigidBody3D] to adjust its mass based on a [CargoHold].
+
+## The cargo hold.
+@export var cargo_hold: CargoHold
+
+@onready var _rigid_body := get_parent() as RigidBody3D
+
+var _added_mass: float = 0.0:
+    set(value):
+        if is_equal_approx(_added_mass, value):
+            return
+        
+        self._rigid_body.mass -= _added_mass
+        _added_mass = value
+        self._rigid_body.mass += _added_mass
+
+func _ready() -> void:
+    self.cargo_hold.changed.connect(_on_cargo_changed)
+
+func _on_cargo_changed() -> void:
+    self._added_mass = self.cargo_hold.get_mass()
