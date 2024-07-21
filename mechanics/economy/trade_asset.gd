650c92cedf710e54251cfb920997462f04eb72d3
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-07-21 14:06:45 +0100

    Uniform reading and adding of commodities and currencies

--- mechanics/economy/trade_asset.gd
+++ mechanics/economy/trade_asset.gd
@@ -11,9 +11,14 @@ class_name TradeAsset
 ## This allows us to use [int]s for computing fractional amounts of this asset, without the precision loss of floating-point math.
 @export var granularity: int = 1
 
+## Returns how much of this asset is in the [CargoHold] or [BankAccount], whichever is appropriate.
+func current_amount(_cargo_hold: CargoHold, _bank_account: BankAccount) -> int:
+    assert(false, "Meant to be implemented by subclasses")
+    return 0
+
 ## Takes up to the specified amount of this asset from a [CargoHold] or [BankAccount], whichever is appropriate.
 ##
-## Returns the amount actually taken, which may be less than the requested if there's not enough.
+## Returns the amount actually taken, which may be less than requested if there's not enough.
 func take_up_to(_amount: int, _cargo_hold: CargoHold, _bank_account: BankAccount) -> int:
     assert(false, "Meant to be implemented by subclasses")
     return 0
@@ -22,6 +27,20 @@ func take_up_to(_amount: int, _cargo_hold: CargoHold, _bank_account: BankAccount
 ##
 ## If [param allow_negative] is set [b]and[/b] this [TradeAsset] is a [Currency], the account can go into negative balance; otherwise, an attempt to take more than what's available will fail and return false.
 func take_exactly(_amount: int, _cargo_hold: CargoHold, _bank_account: BankAccount, _allow_negative: bool=false) -> bool:
+    assert(false, "Meant to be implemented by subclasses")
+    return false
+
+## Adds up to the specified amount of this asset to a [CargoHold] or [BankAccount], whichever is appropriate.
+##
+## Returns the amount actually added, which may be less than requested for commodities if there's not enough room in the cargo hold.
+func add_up_to(_amount: int, _cargo_hold: CargoHold, _bank_account: BankAccount) -> int:
+    assert(false, "Meant to be implemented by subclasses")
+    return 0
+
+## Adds exactly the specified amount of this asset to a [CargoHold] or [BankAccount], whichever is appropriate.
+##
+## If adding to a cargo hold, and there is not enough room, the operation will fail and return false.
+func add_exactly(_amount: int, _cargo_hold: CargoHold, _bank_account: BankAccount) -> bool:
     assert(false, "Meant to be implemented by subclasses")
     return false
 
