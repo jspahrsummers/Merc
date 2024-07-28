606201853fa350125790422f31a5b4ce9a2376f9
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-07-28 14:25:36 +0100

    Trading window refactor

--- screens/landing/trading_window.gd
+++ screens/landing/trading_window.gd
@@ -3,69 +3,48 @@ class_name TradingWindow
 
 @export var market: Market
 
-@export var tab_container: TabContainer
 @export var commodities_container: GridContainer
-@export var currencies_container: GridContainer
 @export var trade_buttons_scene: PackedScene
 
 var cargo_hold: CargoHold
 var bank_account: BankAccount
 
-var _quantity_labels_by_trade_asset: Dictionary = {}
-var _trade_buttons_by_trade_asset: Dictionary = {}
+var _quantity_labels: Dictionary = {}
+var _trade_buttons: Dictionary = {}
 
 func _ready() -> void:
-    var has_commodities := false
-    var has_currencies := false
-
     var money_suffix := market.money.name.to_lower()
 
-    var trade_assets := self.market.trade_assets.keys()
-    trade_assets.sort_custom(func(a: TradeAsset, b: TradeAsset) -> bool: return a.name < b.name)
+    var commodities := self.market.commodities.keys()
+    commodities.sort_custom(func(a: Commodity, b: Commodity) -> bool: return a.name < b.name)
 
-    for trade_asset: TradeAsset in trade_assets:
-        var price: float = self.market.trade_assets[trade_asset]
-
-        var commodity := trade_asset as Commodity
-        var container: GridContainer
-        if commodity:
-            has_commodities = true
-            container = self.commodities_container
-        elif trade_asset is Currency:
-            has_currencies = true
-            container = self.currencies_container
-        else:
-            assert(false, "Unknown trade asset type: %s" % trade_asset)
+    for commodity: Commodity in commodities:
+        var price := self.market.price(commodity)
 
         var name_label := Label.new()
-        name_label.text = trade_asset.name
+        name_label.text = commodity.name
         name_label.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-        container.add_child(name_label)
+        self.commodities_container.add_child(name_label)
 
         var price_label := Label.new()
         price_label.text = "%s %s" % [price, money_suffix]
         price_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
-        container.add_child(price_label)
+        self.commodities_container.add_child(price_label)
 
-        if commodity:
-            var volume_label := Label.new()
-            volume_label.text = "%s m³" % commodity.volume
-            volume_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
-            container.add_child(volume_label)
+        var volume_label := Label.new()
+        volume_label.text = "%s m³" % commodity.volume
+        volume_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_RIGHT
+        self.commodities_container.add_child(volume_label)
 
         var quantity_label := Label.new()
-        self._quantity_labels_by_trade_asset[trade_asset] = quantity_label
-        container.add_child(quantity_label)
+        self._quantity_labels[commodity] = quantity_label
+        self.commodities_container.add_child(quantity_label)
 
         var buttons: TradeButtons = self.trade_buttons_scene.instantiate()
-        buttons.buy_button.pressed.connect(func() -> void: self._buy(trade_asset))
-        buttons.sell_button.pressed.connect(func() -> void: self._sell(trade_asset))
-        self._trade_buttons_by_trade_asset[trade_asset] = buttons
-        container.add_child(buttons)
-
-    self.tab_container.tabs_visible = has_commodities and has_currencies
-    if not has_commodities:
-        self.tab_container.current_tab = 1
+        buttons.buy_button.pressed.connect(func() -> void: self._buy(commodity))
+        buttons.sell_button.pressed.connect(func() -> void: self._sell(commodity))
+        self._trade_buttons[commodity] = buttons
+        self.commodities_container.add_child(buttons)
 
     self.cargo_hold.changed.connect(_update)
     self.bank_account.changed.connect(_update)
@@ -75,48 +54,44 @@ func _on_close_requested() -> void:
     self.visible = false
 
 func _update() -> void:
-    for trade_asset: TradeAsset in self.market.trade_assets:
-        var quantity_label: Label = self._quantity_labels_by_trade_asset[trade_asset]
-        var quantity := trade_asset.current_amount(self.cargo_hold, self.bank_account)
+    for commodity: Commodity in self.market.commodities:
+        var quantity_label: Label = self._quantity_labels[commodity]
+        var quantity := commodity.current_amount(self.cargo_hold, self.bank_account)
         quantity_label.text = "" if is_zero_approx(quantity) else str(quantity)
 
-        var can_pay: bool = self.market.money.current_amount(self.cargo_hold, self.bank_account) - self.market.trade_assets[trade_asset] >= Currency.EPSILON
+        var can_pay: bool = self.market.money.current_amount(self.cargo_hold, self.bank_account) - self.market.commodities[commodity] >= Currency.EPSILON
+        var can_carry := self.cargo_hold.get_occupied_volume() + commodity.volume <= self.cargo_hold.max_volume
 
-        var can_carry: bool = true
-        var commodity := trade_asset as Commodity
-        if commodity:
-            can_carry = self.cargo_hold.get_occupied_volume() + commodity.volume <= self.cargo_hold.max_volume
-
-        var buttons: TradeButtons = self._trade_buttons_by_trade_asset[trade_asset]
+        var buttons: TradeButtons = self._trade_buttons[commodity]
         buttons.buy_button.disabled = not (can_pay and can_carry)
         buttons.sell_button.disabled = quantity == 0
 
-func _buy(trade_asset: TradeAsset) -> void:
-    var asset_price: float = self.market.trade_assets[trade_asset]
+func _buy(commodity: Commodity) -> void:
+    var price := self.market.price(commodity)
     var desired_amount := self._desired_trade_amount()
-    var total_cost: float = asset_price * desired_amount
+    var total_cost: float = price * desired_amount
 
     # Limit to total balance
     var actual_cost := minf(total_cost, self.market.money.current_amount(self.cargo_hold, self.bank_account))
-    var actual_amount := roundf(actual_cost / asset_price)
+    var actual_amount := roundf(actual_cost / price)
 
     # Limit again to capacity
-    actual_amount = trade_asset.add_up_to(actual_amount, self.cargo_hold, self.bank_account)
+    actual_amount = commodity.add_up_to(actual_amount, self.cargo_hold, self.bank_account)
 
-    var withdrew := self.market.money.take_exactly(actual_amount * asset_price, self.cargo_hold, self.bank_account)
+    var withdrew := self.market.money.take_exactly(actual_amount * price, self.cargo_hold, self.bank_account)
     assert(withdrew, "Failed to pay for trade already executed")
 
-func _sell(trade_asset: TradeAsset) -> void:
-    var asset_price: float = self.market.trade_assets[trade_asset]
+func _sell(commodity: Commodity) -> void:
+    var price := self.market.price(commodity)
     var desired_amount := self._desired_trade_amount()
 
     # Limit to total balance
-    var actual_amount := minf(desired_amount, trade_asset.current_amount(self.cargo_hold, self.bank_account))
+    var actual_amount := minf(desired_amount, commodity.current_amount(self.cargo_hold, self.bank_account))
 
     # Limit again to capacity
-    var proceeds := self.market.money.add_up_to(actual_amount * asset_price, self.cargo_hold, self.bank_account)
+    var proceeds := self.market.money.add_up_to(actual_amount * price, self.cargo_hold, self.bank_account)
 
-    var withdrew := trade_asset.take_exactly(roundf(proceeds / asset_price), self.cargo_hold, self.bank_account)
+    var withdrew := commodity.take_exactly(roundf(proceeds / price), self.cargo_hold, self.bank_account)
     assert(withdrew, "Failed to cede asset already sold")
 
 func _desired_trade_amount() -> float:
