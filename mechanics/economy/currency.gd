c45c30d1d6bed46c5e0443d3ce1dfeb368d8365d
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-07-28 15:39:49 +0100

    Customize human-readable rendering per currency

--- mechanics/economy/currency.gd
+++ mechanics/economy/currency.gd
@@ -9,6 +9,9 @@ const EPSILON = 0.001
 ## How many credits one unit of this currency is worth.
 @export var price_in_credits: float
 
+## How many decimal places to show when rendering amounts in this currency.
+@export var precision: int
+
 func current_amount(_cargo_hold: CargoHold, bank_account: BankAccount) -> float:
     return bank_account.currencies.get(self, 0.0)
 
@@ -25,6 +28,9 @@ func add_up_to(amount: float, _cargo_hold: CargoHold, bank_account: BankAccount)
 func add_exactly(amount: float, _cargo_hold: CargoHold, bank_account: BankAccount) -> bool:
     bank_account.deposit(self, amount)
     return true
+
+func amount_as_string(amount: float) -> String:
+    return "%.*f %s" % [self.precision, amount, self.name.to_lower()]
 
 func _to_string() -> String:
     return "Currency:" + self.name
