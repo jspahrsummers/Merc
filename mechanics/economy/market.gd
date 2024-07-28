5c8cd866dce01b348ce7a56fd26edefc98d82a99
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-07-28 14:11:33 +0100

    Market only needs `commodities` now, not currencies (which will be global)

--- mechanics/economy/market.gd
+++ mechanics/economy/market.gd
@@ -8,7 +8,7 @@ class_name Market
 ## This is most commonly a [Currency], but frontier worlds might prefer to trade in physical commodities instead.
 @export var money: TradeAsset
 
-## Any assets that can be traded at the market, structured as a dictionary of [TradeAsset] keys to [float] prices.
+## Any commodities that can be traded at the market, structured as a dictionary of [Commodity] keys to [float] [b]relative[/b] prices.
 ##
-## Prices are units of [member money].
-@export var trade_assets: Dictionary
+## Relative prices are expressed on a scale from 0 to 1, where 0 is extremely cheap relative to the commodity's base price, and 1 is extremely expensive.
+@export var commodities: Dictionary
