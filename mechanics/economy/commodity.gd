145d33dea10562fc6ea8648e7fe219c65103bd63
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-07-28 14:40:45 +0100

    Relative -> absolute price conversion

--- mechanics/economy/commodity.gd
+++ mechanics/economy/commodity.gd
@@ -16,6 +16,28 @@ class_name Commodity
 ## A base price for this commodity, in credits, used to compute relative prices across the galaxy and in other currencies.
 @export var base_price_in_credits: float
 
+## The maximum percentage the price can deviate from the base price for cheap commodities.
+const MAX_PRICE_DEVIATION_CHEAP = 0.5
+
+## The maximum percentage the price can deviate from the base price for expensive commodities.
+const MAX_PRICE_DEVIATION_EXPENSIVE = 0.2
+
+## The price threshold for considering a commodity "expensive".
+const EXPENSIVE_THRESHOLD = 5000.0
+
+## Calculates the actual price, in credits, based on a relative price between 0 and 1.
+func price_in_credits(relative_price: float) -> float:
+    assert(relative_price >= 0.0 and relative_price <= 1.0, "Relative price is out of bounds")
+    
+    var max_deviation := self._max_deviation()
+    var deviation := (relative_price - 0.5) * 2 * max_deviation
+    return self.base_price_in_credits * (1 + deviation)
+
+## Calculates the maximum price deviation based on the commodity's base price.
+func _max_deviation() -> float:
+    var t := minf(self.base_price_in_credits / EXPENSIVE_THRESHOLD, 1.0)
+    return lerp(MAX_PRICE_DEVIATION_CHEAP, MAX_PRICE_DEVIATION_EXPENSIVE, t)
+
 func current_amount(cargo_hold: CargoHold, _bank_account: BankAccount) -> float:
     return cargo_hold.commodities.get(self, 0)
 
--- mechanics/economy/market.gd
+++ mechanics/economy/market.gd
@@ -12,3 +12,18 @@ class_name Market
 ##
 ## Relative prices are expressed on a scale from 0 to 1, where 0 is extremely cheap relative to the commodity's base price, and 1 is extremely expensive.
 @export var commodities: Dictionary
+
+## Returns the absolute price of the given commodity, denominated in units of the market's [member money].
+func price(commodity: Commodity) -> float:
+    var relative_price: float = self.commodities.get(commodity, 0.5)
+    var price_in_credits := commodity.price_in_credits(relative_price)
+
+    var currency := self.money as Currency
+    if currency:
+        return price_in_credits / currency.price_in_credits
+    
+    var commodity_money := self.money as Commodity
+    assert(commodity_money, "Expected money to be a currency or a commodity")
+
+    # TODO: This exchange rate mechanism probably isn't thought-out enough…
+    return price_in_credits / commodity_money.base_price_in_credits
