29936faecc976770fdbd8b5bc5c7c88be39b4e53
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-08-04 16:19:55 +0100

    Error logging

--- screens/game/exit_dialog/exit_dialog.gd
+++ screens/game/exit_dialog/exit_dialog.gd
@@ -3,7 +3,10 @@ extends ConfirmationDialog
 @export var main_menu_scene: PackedScene
 
 func _on_confirmed() -> void:
-    SaveGame.save(self.get_tree(), SaveGame.SAVE_GAMES_DIRECTORY.path_join("autosave.json"))
+    var result := SaveGame.save(self.get_tree(), SaveGame.SAVE_GAMES_DIRECTORY.path_join("autosave.json"))
+    if result != Error.OK:
+        push_error("Failed to save game: %s" % result)
+
     get_tree().change_scene_to_packed(self.main_menu_scene)
 
 func _on_canceled() -> void:
