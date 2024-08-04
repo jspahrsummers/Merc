5c4010a0066623cc0da567b490477fb7f2edc430
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-08-04 21:09:36 +0100

    Persist AI state

--- actors/ai/ai_navigation.gd
+++ actors/ai/ai_navigation.gd
@@ -22,12 +22,14 @@ class_name AINavigation
 @export var auto_start: bool = true
 
 ## State machine for this AI.
+##
+## Note that these values are saved via [SaveGame], so be careful not to break backwards compatibility!
 enum State {
-    IDLE,
-    ROTATING_TO_ACCELERATE,
-    ACCELERATING_TOWARD_DESTINATION,
-    ROTATING_TO_DECELERATE,
-    DECELERATING_TO_STOP,
+    IDLE = 0,
+    ROTATING_TO_ACCELERATE = 1,
+    ACCELERATING_TOWARD_DESTINATION = 2,
+    ROTATING_TO_DECELERATE = 3,
+    DECELERATING_TO_STOP = 4,
 }
 
 var navigating: bool:
@@ -88,7 +90,7 @@ func _accelerate_toward_destination() -> void:
     var stopping_distance := self._calculate_stopping_distance(self._rigid_body.linear_velocity.length())
 
     # Account for rotation time in stopping distance
-    var rotation_time := self._estimate_rotation_time( - self._rigid_body.linear_velocity.normalized())
+    var rotation_time := self._estimate_rotation_time(-self._rigid_body.linear_velocity.normalized())
     var rotation_distance := self._rigid_body.linear_velocity.length() * rotation_time
     stopping_distance += rotation_distance
 
@@ -121,7 +123,7 @@ func _decelerate_to_stop() -> void:
         self.destination_reached.emit(self)
 
 func _pointing_in_direction(direction: Vector3) -> bool:
-    var current_direction := - self._rigid_body.global_transform.basis.z
+    var current_direction := -self._rigid_body.global_transform.basis.z
     return current_direction.angle_to(direction) <= self.direction_tolerance
 
 func _calculate_stopping_distance(current_velocity: float) -> float:
@@ -129,10 +131,24 @@ func _calculate_stopping_distance(current_velocity: float) -> float:
     return (current_velocity * current_velocity) / (2 * acceleration)
 
 func _estimate_rotation_time(target_direction: Vector3) -> float:
-    var current_direction := - self._rigid_body.global_transform.basis.z
+    var current_direction := -self._rigid_body.global_transform.basis.z
     var angle_to_rotate := current_direction.angle_to(target_direction)
     return angle_to_rotate / self.rigid_body_direction.spin_thruster.turning_rate
 
 func set_destination(new_destination: Vector3) -> void:
     self.destination = new_destination
     self.navigating = true
+
+## See [SaveGame].
+func save_to_dict() -> Dictionary:
+    var result := {}
+    result["navigating"] = self.navigating
+    result["state"] = self._state
+    result["target_direction"] = SaveGame.serialize_vector3(self._target_direction)
+    return result
+
+## See [SaveGame].
+func load_from_dict(dict: Dictionary) -> void:
+    self.navigating = dict["navigating"]
+    self._state = dict["state"]
+    self._target_direction = SaveGame.deserialize_vector3(dict["target_direction"])
