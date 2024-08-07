a4cbdceb7cd80abca076f2cb3b45e241a1535d5d
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-08-07 20:48:34 +0100

    Mission resource type

---
+++ mechanics/missions/mission.gd
@@ -0,0 +1,48 @@
+extends SaveableResource
+class_name Mission
+
+## A mission that the player can undertake, with a notion of success and failure.
+
+## The statuses that a mission can be in.
+##
+## Note that these values are saved via [SaveGame], so be careful not to break backwards compatibility!
+enum Status {
+    NOT_ACCEPTED = 0,
+    STARTED = 1,
+    SUCCEEDED = 2,
+    FAILED = 3,
+}
+
+## The human-readable title of the mission.
+@export var title: String
+
+## The human-readable mission description.
+##
+## BBCode can be used to format this description.
+@export var description: String
+
+## A deadline for the mission, in GST cycles (see [Calendar]), or [constant INF] if there is no deadline.
+##
+## If the mission is not completed by this time, it is considered failed.
+@export var deadline_cycle: float = INF:
+    set(value):
+        if is_equal_approx(value, deadline_cycle):
+            return
+        
+        deadline_cycle = value
+        self.emit_changed()
+
+## The current status of the mission.
+@export var status: Status = Status.NOT_ACCEPTED:
+    set(value):
+        if value == status:
+            return
+        
+        status = value
+        self.emit_changed()
+
+## A dictionary of [Commodity] keys to [int] amounts that the player must deliver to complete the mission.
+@export var cargo: Dictionary
+
+## A dictionary of [TradeAsset] keys to [float] amounts that the player will receive upon success.
+@export var monetary_reward: Dictionary
