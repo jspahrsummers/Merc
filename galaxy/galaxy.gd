c76e394144c36061291a4b593b7192cef904a92c
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-08-10 22:02:06 +0100

    Add planets_with_systems lookup from galaxy

--- galaxy/galaxy.gd
+++ galaxy/galaxy.gd
@@ -8,8 +8,16 @@ class_name Galaxy
 ## A list of all systems in the galaxy.
 @export var systems: Array[StarSystem]
 
+## A dictionary of [Planet] keys to their containing [StarSystem] values.
+var planets_with_systems: Dictionary = {}
+
 func _init() -> void:
     self._connect_backref.call_deferred()
+
+    for system in systems:
+        for planet in system.planets:
+            assert(planet not in planets_with_systems, "Planet %s should not exist in multiple systems" % planet)
+            planets_with_systems[planet] = system
 
 func _connect_backref() -> void:
     for system in self.systems:
@@ -20,5 +28,5 @@ func get_system(name: StringName) -> StarSystem:
     for system in systems:
         if system.name == name:
             return system
-    
+
     return null
