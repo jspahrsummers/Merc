f9e88275cde5ad3392139f5284cb8038e5cdbd5d
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-08-19 22:19:21 +0100

    Random bounty selection

--- actors/hero.gd
+++ actors/hero.gd
@@ -7,3 +7,11 @@ class_name Hero
 
 ## The name of the hero.
 @export var name: String
+
+const _BOUNTIES_DIRECTORY = "res://actors/heroes/bounties/"
+
+## Randomly picks a hero that is eligible to have a bounty.
+static func pick_random_bounty() -> Hero:
+    var files := DirAccess.get_files_at(_BOUNTIES_DIRECTORY)
+    var random_file := files[randi_range(0, files.size() - 1)]
+    return load("%s/%s" % [_BOUNTIES_DIRECTORY, random_file])
