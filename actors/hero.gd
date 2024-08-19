8c2942c8c07efa53e689eb8338e8391d5c49150d
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-08-19 22:10:02 +0100

    Introduce `Hero` resource

---
+++ actors/hero.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name Hero
+
+## A "hero" is any named NPC.
+##
+## These are often used as a target or escort in missions.
+
+## The name of the hero.
+@export var name: String
