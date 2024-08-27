e1a6bb56acc09a3d245a3eac22a97e5aaecc0578
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-08-27 17:46:49 +0100

    Show passengers in HUD

--- screens/game/hud/cargo_container.gd
+++ screens/game/hud/cargo_container.gd
@@ -1,17 +1,21 @@
 extends PanelContainer
 
 @export var cargo_hold: CargoHold
+@export var passenger_quarters: PassengerQuarters
 @export var label_container: Container
 @export var free_cargo_label: Label
+@export var passengers_label: Label
 
 var _labels_by_commodity: Dictionary = {}
 
 func _ready() -> void:
     self.cargo_hold.changed.connect(_update)
+    self.passenger_quarters.changed.connect(_update)
     self._update()
 
 func _update() -> void:
     self.label_container.remove_child(self.free_cargo_label)
+    self.label_container.remove_child(self.passengers_label)
 
     var existing_commodities := self._labels_by_commodity.keys()
     for commodity: Commodity in existing_commodities:
@@ -34,3 +38,6 @@ func _update() -> void:
     
     self.free_cargo_label.text = "%s m³ free" % (self.cargo_hold.max_volume - self.cargo_hold.get_occupied_volume())
     self.label_container.add_child(self.free_cargo_label)
+
+    self.passengers_label.text = "%d passenger%s" % [self.passenger_quarters.occupied_spaces, "s" if self.passenger_quarters.occupied_spaces > 1 else ""]
+    self.passengers_label.visible = self.passenger_quarters.occupied_spaces > 0
