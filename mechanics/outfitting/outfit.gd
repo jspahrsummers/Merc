b216c9e5d6152602a1abdd51f5c4bda9eafa4d3d
Justin Spahr-Summers <justin@jspahrsummers.com>
2024-09-02 15:43:03 +0100

    Allow all outfit properties to have positive or negative effects

--- mechanics/outfitting/outfit.gd
+++ mechanics/outfitting/outfit.gd
@@ -7,6 +7,7 @@ class_name Outfit
 ## - Weapons are limited by the number of weapon mounts (hardpoints) available.
 ## - Outfits add to a ship's mass, reducing its ability to accelerate and manuever.
 ## - Outfits may require constant power, which is limited by the ship's power generator.
+## - Outfits may consume cargo space, which could otherwise be used to carry more trade or mission commodities.
 
 # TODO: Heat
 
@@ -22,22 +23,28 @@ class_name Outfit
 ## The price of this outfit in credits.
 @export var price_in_credits: float
 
-## Additional cargo capacity provided, in cubic meters.
-@export var additional_cargo_capacity: float = 0
+## A modification this outfit makes to cargo capacity, in cubic meters.
+##
+## If negative, the capacity of the ship's [CargoHold] will be reduced. The outfit cannot be installed if the available space in the cargo hold is insufficient.
+@export var modified_cargo_capacity: float = 0
 
-## Additional fuel capacity provided.
-@export var additional_fuel_capacity: float = 0
+## A modification this outfit makes to fuel capacity.
+##
+## If negative, the fuel capacity of the ship's [Hyperdrive] will be reduced. The outfit cannot be installed if the available fuel capacity is insufficient.
+@export var modified_fuel_capacity: float = 0
 
-## Additional shield capacity provided.
-@export var additional_shield_capacity: float = 0
+## A modification this outfit makes to shield capacity.
+##
+## If negative, the maximum integrity of the ship's [Shield] will be reduced. The outfit cannot be installed if the maximum shield integrity is insufficient.
+@export var modified_shield_capacity: float = 0
 
 ## A multiplier to apply to shield recharge rate.
 @export var shield_recharge_multiplier: float = 1.0
 
-## A rate of power per second required to power this outfit.
+## A modification to the ship's power generation, as an amount added or removed per second.
 ##
-## The output of the ship's [PowerGenerator] will be reduced by this amount. The outfit cannot be installed if the [PowerGenerator] is insufficient.
-@export var constant_power_drain: float = 0
+## If negative, the output of the ship's [PowerGenerator] will be reduced by this amount. The outfit cannot be installed if the [PowerGenerator] is insufficient.
+@export var modified_power_generation: float = 0
 
 ## A weapon provided by this outfit.
 @export var weapon: Weapon
@@ -47,6 +54,39 @@ const REFUND_PERCENTAGE = 0.75
 
 ## Whether this outfit is able to be installed onto the given ship.
 func can_install_onto_ship(ship: Ship) -> bool:
+    if not is_zero_approx(self.modified_cargo_capacity):
+        var cargo_hold := ship.cargo_hold
+        if not cargo_hold:
+            return false
+        
+        var available_volume := cargo_hold.max_volume - cargo_hold.get_occupied_volume()
+        if available_volume + self.modified_cargo_capacity < 0:
+            return false
+    
+    if not is_zero_approx(self.modified_fuel_capacity):
+        var hyperdrive := ship.hyperdrive
+        if not hyperdrive:
+            return false
+        
+        if hyperdrive.max_fuel + self.modified_fuel_capacity < 0:
+            return false
+        
+    if not is_zero_approx(self.modified_shield_capacity):
+        var shield := ship.shield
+        if not shield:
+            return false
+        
+        if shield.max_integrity + self.modified_shield_capacity < 0:
+            return false
+    
+    if not is_zero_approx(self.modified_power_generation):
+        var generator := ship.power_management_unit.power_generator
+        if not generator:
+            return false
+        
+        if generator.rate_of_power < self.modified_power_generation:
+            return false
+
     if self.weapon:
         var mount_available := false
         for mount in ship.weapon_mounts:
@@ -56,21 +96,27 @@ func can_install_onto_ship(ship: Ship) -> bool:
         
         if not mount_available:
             return false
-    
-    if not is_zero_approx(self.constant_power_drain):
-        var generator := ship.power_management_unit.power_generator
-        if not generator:
-            return false
-        
-        if generator.rate_of_power < self.constant_power_drain:
-            return false
-    
-    # TODO: Limit other types of outfits (e.g., based on hardpoints), to prevent infinite accumulation of benefits.
+
     return true
 
 ## Apply the effects of this outfit to a ship, or returns false if the outfit cannot be installed.
 func apply_to_ship(ship: Ship) -> void:
     assert(self.can_install_onto_ship(ship), "Outfit is not installable onto the ship")
+
+    ship.mass += self.mass
+
+    if ship.cargo_hold:
+        ship.cargo_hold.max_volume += self.modified_cargo_capacity
+
+    if ship.hyperdrive:
+        ship.hyperdrive.max_fuel += self.modified_fuel_capacity
+
+    if ship.shield:
+        ship.shield.max_integrity += self.modified_shield_capacity
+        ship.shield.recharge_rate *= self.shield_recharge_multiplier
+    
+    if ship.power_management_unit.power_generator:
+        ship.power_management_unit.power_generator.rate_of_power += self.modified_power_generation
 
     if self.weapon:
         var weapon_mount: WeaponMount = null
@@ -81,50 +127,29 @@ func apply_to_ship(ship: Ship) -> void:
         
         assert(weapon_mount, "Could not find weapon mount")
         weapon_mount.weapon = self.weapon
-    
-    if not is_zero_approx(self.constant_power_drain):
-        var generator := ship.power_management_unit.power_generator
-        assert(generator, "Could not find power generator")
-        
-        generator.rate_of_power -= self.constant_power_drain
-
-    ship.mass += self.mass
-
-    if ship.cargo_hold:
-        ship.cargo_hold.max_volume += self.additional_cargo_capacity
-
-    if ship.hyperdrive:
-        ship.hyperdrive.max_fuel += self.additional_fuel_capacity
-
-    if ship.shield:
-        ship.shield.max_integrity += self.additional_shield_capacity
-        ship.shield.recharge_rate *= self.shield_recharge_multiplier
 
 ## Remove the effects of this outfit from a ship.
 func remove_from_ship(ship: Ship) -> void:
     ship.mass -= self.mass
 
     if ship.cargo_hold:
-        ship.cargo_hold.max_volume -= self.additional_cargo_capacity
+        ship.cargo_hold.max_volume -= self.modified_cargo_capacity
 
     if ship.hyperdrive:
-        ship.hyperdrive.max_fuel -= self.additional_fuel_capacity
+        ship.hyperdrive.max_fuel -= self.modified_fuel_capacity
 
     if ship.shield:
-        ship.shield.max_integrity -= self.additional_shield_capacity
+        ship.shield.max_integrity -= self.modified_shield_capacity
         ship.shield.recharge_rate /= self.shield_recharge_multiplier
     
+    if ship.power_management_unit.power_generator:
+        ship.power_management_unit.power_generator.rate_of_power -= self.modified_power_generation
+
     if self.weapon:
         for mount in ship.weapon_mounts:
             if mount.weapon == self.weapon:
                 mount.weapon = null
                 break
-    
-    if not is_zero_approx(self.constant_power_drain):
-        var generator := ship.power_management_unit.power_generator
-        assert(generator, "Could not find power generator")
-        
-        generator.rate_of_power += self.constant_power_drain
 
 ## Returns the list of effects provided by this outfit, as BBCode formatted strings.
 func get_effects() -> PackedStringArray:
@@ -133,17 +158,20 @@ func get_effects() -> PackedStringArray:
     if not is_zero_approx(self.mass):
         effects.push_back("[b]Mass:[/b] %s kg" % self.mass)
 
-    if not is_zero_approx(self.constant_power_drain):
-        effects.push_back("[b]Power requirement:[/b] %s/s" % self.constant_power_drain)
+    if not is_zero_approx(self.modified_power_generation):
+        if self.modified_power_generation > 0:
+            effects.push_back("[b]Power generation:[/b] %s/s" % self.modified_power_generation)
+        else:
+            effects.push_back("[b]Power requirement:[/b] %s/s" % -self.modified_power_generation)
 
-    if not is_zero_approx(self.additional_cargo_capacity):
-        effects.push_back("[b]Additional cargo capacity:[/b] %s m³" % self.additional_cargo_capacity)
+    if not is_zero_approx(self.modified_cargo_capacity):
+        effects.push_back("[b]Cargo capacity:[/b] %s m³" % self._signed_string(self.modified_cargo_capacity))
 
-    if not is_zero_approx(self.additional_fuel_capacity):
-        effects.push_back("[b]Additional fuel capacity:[/b] %s hyperjumps" % self.additional_fuel_capacity)
+    if not is_zero_approx(self.modified_fuel_capacity):
+        effects.push_back("[b]Fuel capacity:[/b] %s" % self._signed_string(self.modified_fuel_capacity))
 
-    if not is_zero_approx(self.additional_shield_capacity):
-        effects.push_back("[b]Additional shield capacity:[/b] %s" % self.additional_shield_capacity)
+    if not is_zero_approx(self.modified_shield_capacity):
+        effects.push_back("[b]Shield capacity:[/b] %s" % self._signed_string(self.modified_shield_capacity))
 
     if not is_equal_approx(self.shield_recharge_multiplier, 1.0):
         effects.push_back("[b]Shield recharge rate:[/b] %s%%" % [self.shield_recharge_multiplier * 100])
@@ -153,3 +181,11 @@ func get_effects() -> PackedStringArray:
         effects.push_back("[b]Power consumption per shot:[/b] %s" % [self.weapon.power_consumption])
 
     return effects
+
+func _signed_string(value: float) -> String:
+    if is_zero_approx(value):
+        return "0"
+    elif value < 0:
+        return "%s" % value
+    else:
+        return "+%s" % value
